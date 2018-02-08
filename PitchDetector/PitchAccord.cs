ea333a60f62e1a80c4a5fb6c3976beb10c2bc267
azyobuzin <azyobuzin@users.sourceforge.jp>
2018-02-09 04:25:43 +0900

    放物線補間をちゃんと展開してみた

--- PitchDetector/PitchAccord.cs
+++ PitchDetector/PitchAccord.cs
@@ -62,29 +62,48 @@ namespace PitchDetector
             var peaks = new List<PeakPoint>();
             for (var i = 1; i < samples.Length; i++)
             {
-                if (nsdf[i] > 0 && nsdf[i - 1] <= 0)
+                if (nsdf[i] >= 0) continue; // 最初に負になるところまでスキップ
+
+                i++;
+                for (; i < samples.Length; i++)
                 {
-                    var currentMax = new PeakPoint(i, nsdf[i]);
-                    i++;
-
-                    for (; i < samples.Length; i++)
+                    if (nsdf[i] > 0)
                     {
-                        if (nsdf[i] > currentMax.Correlation)
-                        {
-                            currentMax.Delay = i;
-                            currentMax.Correlation = nsdf[i];
-                        }
-                        else if (nsdf[i] < 0)
-                        {
-                            // 0 未満になったので一山終了
-                            break;
-                        }
-                    }
+                        var currentMaxIndex = i;
+                        var currentMax = nsdf[i];
+                        i++;
 
-                    peaks.Add(currentMax);
-                    if (currentMax.Correlation > maxCorrelation)
-                        maxCorrelation = currentMax.Correlation;
+                        for (; i < samples.Length; i++)
+                        {
+                            if (nsdf[i] > currentMax)
+                            {
+                                currentMaxIndex = i;
+                                currentMax = nsdf[i];
+                            }
+                            else if (nsdf[i] < 0)
+                            {
+                                // 0 未満になったので一山終了
+                                break;
+                            }
+                        }
+
+                        var peak = currentMaxIndex < nsdf.Length - 1
+                            && Interporate(
+                                currentMaxIndex - 1, nsdf[currentMaxIndex - 1],
+                                currentMaxIndex, currentMax,
+                                currentMaxIndex + 1, nsdf[currentMaxIndex + 1]
+                            ) is ValueTuple<double, double> interpolated
+                            ? new PeakPoint(interpolated.Item1, interpolated.Item2)
+                            : new PeakPoint(currentMaxIndex, currentMax);
+
+                        peaks.Add(peak);
+
+                        if (peak.Correlation > maxCorrelation)
+                            maxCorrelation = peak.Correlation;
+                    }
                 }
+
+                break;
             }
 
             if (peaks.Count == 0) return null; // 推定失敗
@@ -92,50 +111,41 @@ namespace PitchDetector
             var threshold = maxCorrelation * 0.8;
             var mainPeak = peaks.Find(x => x.Correlation >= threshold);
 
-            var delay = mainPeak.Delay == nsdf.Length - 1
-                ? mainPeak.Delay
-                : GetTopX(
-                    mainPeak.Delay - 1, nsdf[mainPeak.Delay - 1],
-                    mainPeak.Delay, mainPeak.Correlation,
-                    mainPeak.Delay + 1, nsdf[mainPeak.Delay + 1]
-                );
-
-            Debug.Assert(delay >= mainPeak.Delay - 1 && delay <= mainPeak.Delay + 1);
-            return sampleRate / delay;
+            return sampleRate / mainPeak.Delay;
         }
 
         /// <summary>
-        /// 2次関数として放物線補間して頂点のX座標を求める
+        /// 2次関数として放物線補間して頂点の座標を求める
         /// </summary>
-        private static double GetTopX(double x1, double y1, double x2, double y2, double x3, double y3)
+        private static (double, double)? Interporate(double x1, double y1, double x2, double y2, double x3, double y3)
         {
-            // y = ax^2 + bx + c
+            // y = ax^2 + bx + c として、 a, b, c を解く
+            var x12 = x1 * x1;
+            var x22 = x2 * x2;
+            var x32 = x3 * x3;
 
-            var m = new[,]
-            {
-                { x1 * x1, x1, 1.0 },
-                { x2 * x2, x2, 1.0 },
-                { x3 * x3, x3, 1.0 }
-            };
+            var det = x12 * (x2 - x3) + x22 * (x3 - x1) + x32 * (x1 - x2);
+            if (det == 0) return null;
 
-            // 行列で連立方程式を解くやつ
-            var solution = m.Inverse(true).Dot(new[,] { { y1 }, { y2 }, { y3 } });
-
-            var a = solution[0, 0];
-            var b = solution[1, 0];
-            //var c = solution[2, 0];
+            var a = (y1 * (x2 - x3) + y2 * (x3 - x1) + y3 * (x1 - x2)) / det;
+            var b = (y1 * (x32 - x22) + y2 * (x12 - x32) + y3 * (x22 - x12)) / det;
+            var c = (y1 * (x22 * x3 - x32 * x2) + y2 * (x32 * x1 - x12 * x3) + y3 * (x12 * x2 - x22 * x1)) / det;
 
             // 頂点の公式
-            return -b / (2.0 * a);
+            var x = -b / (2.0 * a);
+
+            Debug.Assert(x >= x1 && x <= x3);
+
+            return (x, a * x * x + b * x + c);
         }
 
         [StructLayout(LayoutKind.Auto)]
         private struct PeakPoint
         {
-            public int Delay { get; set; }
+            public double Delay { get; set; }
             public double Correlation { get; set; }
 
-            public PeakPoint(int delay, double correlation)
+            public PeakPoint(double delay, double correlation)
             {
                 this.Delay = delay;
                 this.Correlation = correlation;
